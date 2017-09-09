702c1d5f753e574f314892f89ccbfd679fbe6635
hgf <957333489@qq.com>
2017-09-09 20:41:39 +0800

    提取模板头部

---
+++ ttsx/df_user/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def register(request):
+
+    title = '天天生鲜-注册'
+    context = {"title":title}
+    return render(request,'df_user/register.html',context)
+
+
+def login(request):
+    title = '天天生鲜-登录'
+    context = {"title":title}
+    return render(request,'df_user/login.html',context)
+

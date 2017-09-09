7a7e528bf6e1024dc63dc8560b8c70382aed1351
hgf <957333489@qq.com>
2017-09-09 21:00:15 +0800

    解决冲突

--- ttsx/df_user/urls.py
+++ ttsx/df_user/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns=[
-<<<<<<< HEAD
     url(r'^register/$',views.register),
     url(r'^login/$',views.login),
 
-=======
-    url(r'^register/$',views.register)
->>>>>>> b417c8dcf663c3856e66a79dc4c8d85ab07cfd3a
 ]

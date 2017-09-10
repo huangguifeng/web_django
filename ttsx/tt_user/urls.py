e30ebe32fe1e1655e000e5610ac39e0c8ad8a504
hgf <957333489@qq.com>
2017-09-10 12:28:49 +0800

    修改模板

--- ttsx/tt_user/urls.py
+++ ttsx/tt_user/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns=[
     url(r'^register/$',views.register),
-    url(r'^login/$',views.login),
+    url(r'^list/$',views.list),
 
 ]
--- ttsx/tt_user/views.py
+++ ttsx/tt_user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from PIL import Image, ImageDraw, ImageFont
+import random
 # Create your views here.
 
 def register(request):
@@ -15,3 +16,45 @@ def login(request):
     context = {"title":title}
     return render(request, 'tt_user/login.html', context)
 
+
+def list(request):
+    return render(request, 'tt_goods/list.html')
+
+
+def verifycode(request):
+    """随机生成6位的验证码（字母数字随机组合，包含大小写）"""
+
+    code_list = []
+    # 每一位验证码都有三种可能（大写字母，小写字母，数字）
+    for i in range(6):
+        statu = random.randint(1,3)
+        if statu == 1:
+            a = random.randint(65,90)
+            random_uppercase = chr(a)
+            code_list.append(random_uppercase)
+
+        elif statu == 2:
+            b = random.randint(97,122)
+            random_lowercase = chr(b)
+            code_list.append(random_lowercase)
+
+        elif statu == 3:
+            random_num = random.randint(0,9)
+            code_list.append(str(random_num))
+
+    verification_code = "".join(code_list)
+    font = ImageFont.truetype('FreeMono.ttf', 23)  # 字体
+    bgcolor = (random.randrange(20, 100), random.randrange(
+        20, 100), random.randrange(10,255))
+    height = 100
+    width = 25
+    # 创建画布
+    im = Image.new('RGB', (width, height), bgcolor)
+    # 创建画笔对象　
+    draw = ImageDraw.Draw(im)
+
+    # 噪点
+    for i in range(0, 100):
+        xy = (random.randrange(0, width), random.randrange(0, height))
+        fill = (random.randrange(0, 255), 255, random.randrange(0, 255))
+        draw.point(xy, fill=fill)

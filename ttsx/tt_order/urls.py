d3395316c5e3faebfec2eaae57141e8f9e128595
zhangfan <441515221@qq.com>
2017-09-13 11:54:02 +0800

    订单

---
+++ ttsx/tt_order/urls.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+#coding=utf-8
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns=[
+    url(r'^tt_order/$',views.orderGoods),
+    url(r'^refer_order/$',views.referOrder)
+]
--- ttsx/ttsx/urls.py
+++ ttsx/ttsx/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^',include('tt_goods.urls')),
     url(r'^user/', include('tt_user.urls')),
+    url(r'^order/',include('tt_order.urls')),
 ]

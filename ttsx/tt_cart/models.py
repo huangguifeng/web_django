ade326ed6c6ebfd8d62ac90526dbe2b45ce1091a
chuantian <qwpoiu123@qq.com>
2017-09-13 15:49:50 +0800

    迁移初始化

--- ttsx/tt_cart/models.py
+++ ttsx/tt_cart/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 # Create your models here.
 
-# class CartInfo(models.Model):
-#     # 用户
-#     user = models.ForeignKey('tt_user.UserInfo')
-#     # 商品
-#     goods = models.ForeignKey('tt_goods.GoodsInfo')
-#     # 数量
-#     count = models.IntegerField()
+class CartInfo(models.Model):
+    # 用户
+    user = models.ForeignKey('tt_user.UserInfo')
+    # 商品
+    goods = models.ForeignKey('tt_goods.GoodsInfo')
+    # 数量
+    count = models.IntegerField()

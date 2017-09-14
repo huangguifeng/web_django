2e89ac3c1dcbf142ae1987c4aed9b3549e2cebf1
chuantian <qwpoiu123@qq.com>
2017-09-14 10:49:41 +0800

    购物车9-14早上 添

--- ttsx/tt_cart/urls.py
+++ ttsx/tt_cart/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'cart.html$', views.carts),
+    url(r'cart.html$', views.cartSum),
+    # url(r'abc.html$', views.carts),
 ]
--- ttsx/tt_cart/views.py
+++ ttsx/tt_cart/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from tt_cart.models import *
+from tt_goods.models import *
+from django.http import HttpResponse
 
 # Create your views here.
 
-def carts(request):
-    return render(request, 'tt_cart/cart.html')
+# def carts(request):
+    # return render(request, 'tt_cart/abc.html', abc)
+
+def cartSum(request):
+    goods_obj = GoodsInfo.objects.filter(id__gt=26).filter(id__lt=31)
+    goods = {'goods':goods_obj}
+    return render(request, 'tt_cart/cart.html', goods)

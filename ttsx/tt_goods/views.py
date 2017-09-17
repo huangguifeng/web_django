b88ec69ab94482a1124138dc3625d57d52a67e67
mhd <806595012@qq.com>
2017-09-17 14:46:00 +0800

    wuyu

--- ttsx/tt_goods/views.py
+++ ttsx/tt_goods/views.py
@@ -1,5 +1,7 @@
+
+
 from django.shortcuts import render
-from .models import *
+from tt_goods.models import *
 from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
